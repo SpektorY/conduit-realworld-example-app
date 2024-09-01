963ce8d1d611a6acf6224bad3efd8131bad98d79
SpektorY <165672672+SpektorY@users.noreply.github.com>
2024-09-01 18:10:18 +0300

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/authentication");
-const { currentUser, updateUser } = require("../controllers/user");
+const { fetchCurrentUser, updateUser } = require("../controllers/userController");
 
 //* Current User
-router.get("/", verifyToken, currentUser);
+router.get("/", verifyToken, fetchCurrentUser);
+
 //* Update User
 router.put("/", verifyToken, updateUser);
 
